519f61e75f0a646660024ae52e992d16cf825498
Sam Hollyer <rsamhollyer@gmail.com>
2021-02-01 13:58:03 -0600

    place holder graphs injected into dashboard

--- src/Components/AltitudeGraph.jsx
+++ src/Components/AltitudeGraph.jsx
@@ -1,7 +1,24 @@
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
+
 const AltitudeGraph = () => {
 	return (
 		<div className="alt-graph-comp">
 			<h2>Alt. Graph</h2>
+			<VictoryChart theme={VictoryTheme.material}>
+				<VictoryLine
+					style={{
+						data: { stroke: "#c43a31" },
+						parent: { border: "1px solid #ccc" },
+					}}
+					data={[
+						{ x: 1, y: 2 },
+						{ x: 2, y: 3 },
+						{ x: 3, y: 5 },
+						{ x: 4, y: 4 },
+						{ x: 5, y: 7 },
+					]}
+				/>
+			</VictoryChart>
 		</div>
 	);
 };
--- src/Components/SpeedGraph.jsx
+++ src/Components/SpeedGraph.jsx
@@ -1,7 +1,25 @@
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
+
 const SpeedGraph = () => {
 	return (
 		<div className="speed-graph-component">
 			<h2>Speed Graph</h2>
+
+			<VictoryChart theme={VictoryTheme.material}>
+				<VictoryLine
+					style={{
+						data: { stroke: "#c43a31" },
+						parent: { border: "1px solid #ccc" },
+					}}
+					data={[
+						{ x: 1, y: 2 },
+						{ x: 2, y: 3 },
+						{ x: 3, y: 5 },
+						{ x: 4, y: 4 },
+						{ x: 5, y: 7 },
+					]}
+				/>
+			</VictoryChart>
 		</div>
 	);
 };
