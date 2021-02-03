4586e530e3563444d7c6ff567940411bed8c809a
Sam Hollyer <rsamhollyer@gmail.com>
2021-02-03 10:58:55 -0600

    added a button that loads the flight data and calls the weather api

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,17 @@
 import Moment from "react-moment";
-const Header = () => {
+const Header = (props) => {
+	const { getWeather, getData } = props;
 	return (
 		<div className="header-component">
 			<h1>Drone Flight Recorder</h1>
+			<button
+				onClick={() => {
+					getWeather("imperial", "77532");
+					getData();
+				}}
+			>
+				Get Flight
+			</button>
 			<div className="date-time">
 				<Moment className="date" format="DD/MM/YYYY" />
 				<Moment className="time" format="HH:mm" />
