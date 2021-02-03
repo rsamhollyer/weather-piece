51d8d4b0447bd01749b5753efac442fb5822f898
Sam Hollyer <rsamhollyer@gmail.com>
2021-02-03 15:31:44 -0600

    still working on graph

--- src/Components/SpeedGraph.jsx
+++ src/Components/SpeedGraph.jsx
@@ -1,17 +1,14 @@
 import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SpeedGraph = (props) => {
 	const { chartData } = props;
 	const [chartAxis, setChartAxis] = useState([{ x: 0, y: 0 }]);
-
-	console.log(`CHARTDATA ${chartData}`);
-	console.log(`CHARTAXIS ${chartAxis}`);
-
-	if (chartData.length > 1) {
-		setChartAxis([...chartAxis, { x: chartData.speed, y: chartData.time }]);
-	}
-
+	let arr = [];
+	/*
+	Get chart data [{},{},{}....] 
+	Loop thru and push the time and speed value out as it updates and pass them down to the graph data component
+	*/
 	return (
 		<div className="speed-graph-component">
 			<h2>Speed Graph</h2>
