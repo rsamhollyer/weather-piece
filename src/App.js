40a667f55970a4b66b81821bb29ade0bbec5df54
Sam Hollyer <rsamhollyer@gmail.com>
2021-02-03 10:09:22 -0600

    trying to send data to single stat component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,19 @@ import Map from "./Components/Map";
 import { config } from "./Config/config";
 
 function App() {
+	useEffect(() => {
+		getWeather("imperial", "77532");
+		getData();
+		setDataLength(data.length);
+	}, []);
+
 	const [weather, setWeather] = useState(null);
+	const [data, setData] = useState([]);
+	const [index, setIndex] = useState(0);
+	const [dataLength, setDataLength] = useState(0);
+
+	let currentData = data[index] || {};
+
 	const [viewport, setViewport] = useState({
 		latitude: 29.910577,
 		longitude: -95.060882,
@@ -22,8 +34,6 @@ function App() {
 		width: "90%",
 		height: "90%",
 	});
-
-	const [data, setData] = useState([]);
 
 	const getWeather = async (units, zipcode) => {
 		const resp = await axios.get(
@@ -33,25 +43,33 @@ function App() {
 	};
 
 	const getData = async () => {
-		const res = await axios.get("data/2013_04_14_merlischachen.kml.json");
-		setData(res.data);
+		const resp = await axios.get("data/2013_04_14_merlischachen.kml.json");
+		setData(resp.data);
 	};
 
-	useEffect(() => {
-		getWeather("imperial", "94203");
-	}, []);
+	console.log(`DATA ${data}`);
+	console.log(`INDEX ${index}`);
+	console.log(`LENGTH ${dataLength}`);
 
 	return (
 		<main className="app">
 			<Header />
-			<Speed />
+			<Speed data={currentData} />
 			<Altitude />
 			<MotorTemp />
 			<FlightDuration />
 			<SpeedGraph />
 			<AltitudeGraph />
 			<MotorGraph />
-
+			<button
+				style={{ fontSize: "3rem" }}
+				onClick={(e) => {
+					e.preventDefault();
+					return index < dataLength - 1 ? setIndex(index + 1) : setIndex(0);
+				}}
+			>
+				CLICK ME
+			</button>
 			<Map viewport={viewport} setViewport={setViewport} />
 
 			<Weather weather={weather} />
--- src/Components/Speed.jsx
+++ src/Components/Speed.jsx
@@ -1,10 +1,13 @@
-const Speed = () => {
+const Speed = (props) => {
+	const { data } = props;
+	console.log(data);
 	return (
 		<div className="speed-component single-stat-panel">
 			<div className="text-area">
 				<h3 className="component-header">Speed</h3>
 				<h2 className="read-out-info">
-					200 <span>mph</span>
+					{data.speed ? data.speed : 0}
+					<span>mph</span>
 				</h2>
 			</div>
 
