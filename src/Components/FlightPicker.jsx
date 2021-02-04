0ec253a41cea2424403836fbb14c01490a540ddc
Sam Hollyer <rsamhollyer@gmail.com>
2021-02-04 13:43:57 -0600

    updated flight picker to choose which drone to fly

--- src/Components/FlightPicker.jsx
+++ src/Components/FlightPicker.jsx
@@ -1,3 +1,42 @@
-export default function FlightPicker() {
-	return <div className="picker-component"></div>;
+export default function FlightPicker(props) {
+	const { getData } = props;
+	return (
+		<div className="picker-component">
+			<button
+				onClick={() => {
+					getData(1);
+				}}
+			>
+				Get Drone 1
+			</button>
+			<button
+				onClick={() => {
+					getData(2);
+				}}
+			>
+				Get Drone 2
+			</button>
+			<button
+				onClick={() => {
+					getData(3);
+				}}
+			>
+				Get Drone 3
+			</button>
+			<button
+				onClick={() => {
+					getData(4);
+				}}
+			>
+				Get Drone 4
+			</button>
+			<button
+				onClick={() => {
+					getData(5);
+				}}
+			>
+				Get Drone 5
+			</button>
+		</div>
+	);
 }
